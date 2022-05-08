5015bd0f81c8c12ecb531063a9142c4f35f6d32a
Abel Menezes <abelsmenezes@gmail.com>
2022-05-08 20:00:06 -0300

    adicionando migrações criadas pelo sequelize

---
+++ migrations/20220508215942-create-cliente.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Clientes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      cpf: {
+        type: Sequelize.STRING
+      },
+      dtNascimento: {
+        type: Sequelize.DATE
+      },
+      nomeMae: {
+        type: Sequelize.STRING
+      },
+      endereco: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Clientes');
+  }
+};
