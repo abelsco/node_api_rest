68a8e40991bb2da62750abc5c82042bb06262844
Abel Menezes <abelsmenezes@gmail.com>
2022-05-08 20:28:28 -0300

    atualizando o script de inicialização

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-npx sequelize model:create --name Cliente --attributes 'nome:string,cpf:string,dtNascimento:date,nomeMae:string,endereco:string' --force
-npx sequelize model:create --name Exame --attributes 'protocolo:Integer,descricao:string' --force
-npx sequelize model:create --name Plano --attributes 'nome:String,numCarteirinha:integer' --force
+npx sequelize model:create --name Cliente --attributes 'nome:string,cpf:string,dtNascimento:date,nomeMae:string,endereco:string'
+npx sequelize model:create --name Exame --attributes 'protocolo:Integer,descricao:string'
+npx sequelize model:create --name Plano --attributes 'nome:String,numCarteirinha:integer'
 npx sequelize db:create
 npx sequelize db:migrate
+yarn run dev
